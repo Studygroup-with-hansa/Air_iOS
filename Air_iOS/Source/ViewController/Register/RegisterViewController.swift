39023305ad7f1340ee20b8fb59b99647e401c5a3
Skyline-23 <bs2740@naver.com>
2021-09-28 14:01:24 +0900

    [ADD] add RegisterViewController

---
+++ Air_iOS/Source/ViewController/Register/RegisterViewController.swift
@@ -0,0 +1,60 @@
+//
+//  RegisterViewController.swift
+//  Air_iOS
+//
+//  Created by 김부성 on 2021/09/28.
+//
+
+import UIKit
+
+import ReactorKit
+
+final class RegisterViewController: BaseViewController, View {
+    
+    typealias Reactor = RegisterViewReactor
+    
+    // MARK: - Constants
+    fileprivate struct Metric {
+        
+    }
+    
+    fileprivate struct Font {
+        
+    }
+    
+    // MARK: - Properties
+    
+    // MARK: - UI
+    
+    // MARK: - Inintializing
+    init(reactor: Reactor) {
+        super.init()
+        defer {
+            self.reactor = reactor
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        
+    }
+    
+    override func setupConstraints() {
+        super.setupConstraints()
+        
+    }
+    
+    // MARK: - Configuring
+    func bind(reactor: RegisterViewReactor) {
+        
+    }
+}
