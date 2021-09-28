607a6450a4d64b56224cf63e8e4bd6584849d675
Skyline-23 <bs2740@naver.com>
2021-09-28 15:00:57 +0900

    [ADD] add background and logo

--- Air_iOS/Source/ViewController/Register/RegisterViewController.swift
+++ Air_iOS/Source/ViewController/Register/RegisterViewController.swift
@@ -15,7 +15,10 @@ final class RegisterViewController: BaseViewController, View {
     
     // MARK: - Constants
     fileprivate struct Metric {
-        
+        // logo
+        static let logoHeight = 40.f
+        static let logoWidth = 36.f
+        static let logoBottom = 25.f
     }
     
     fileprivate struct Font {
@@ -25,6 +28,14 @@ final class RegisterViewController: BaseViewController, View {
     // MARK: - Properties
     
     // MARK: - UI
+    let backgroundView = UIView().then {
+        $0.backgroundColor = R.color.mainColor()
+    }
+    
+    let logoImage = UIImageView().then {
+        $0.image = R.image.air_logo()
+        $0.tintColor = .white
+    }
     
     // MARK: - Inintializing
     init(reactor: Reactor) {
@@ -46,11 +57,24 @@ final class RegisterViewController: BaseViewController, View {
     override func setupLayout() {
         super.setupLayout()
         
+        self.view.addSubview(self.backgroundView)
+        self.backgroundView.addSubview(self.logoImage)
     }
     
     override func setupConstraints() {
         super.setupConstraints()
         
+        self.backgroundView.snp.makeConstraints {
+            $0.top.right.left.equalToSuperview()
+            $0.height.equalToSuperview().dividedBy(6)
+        }
+        
+        self.logoImage.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().offset(-Metric.logoBottom)
+            $0.height.equalTo(Metric.logoHeight)
+            $0.width.equalTo(Metric.logoWidth)
+        }
     }
     
     // MARK: - Configuring
