f845ebfaadba20d4237d80ae045f508d11748972
Skyline-23 <bs2740@naver.com>
2021-09-29 15:01:07 +0900

    [ADD] add GraphViewController

---
+++ Air_iOS/Source/ViewController/Graph/GraphViewController.swift.swift
@@ -0,0 +1,60 @@
+//
+//  GraphViewController.swift.swift
+//  Air_iOS
+//
+//  Created by 김부성 on 2021/09/29.
+//
+
+import UIKit
+
+import ReactorKit
+
+final class GraphViewController: BaseViewController, View {
+    
+    typealias Reactor = GraphViewReactor
+    
+    // MARK: - Constants
+    fileprivate struct Metric {
+        
+    }
+    
+    fileprivate struct Font {
+        
+    }
+    
+    // MARK: - Properties
+    
+    // MARK: - UI
+    
+    // MARK: - Inintializing
+    init(reactor: Reactor) {
+        super.init()
+        defer {
+            self.reactor = reactor
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        
+    }
+    
+    override func setupConstraints() {
+        super.setupConstraints()
+        
+    }
+    
+    // MARK: - Configuring
+    func bind(reactor: GraphViewReactor) {
+        
+    }
+}
